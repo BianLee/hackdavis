696944ae19c67fcfe43bd047f82ddb2701f03b7e
Bian Lee <bianlee2004@gmail.com>
2023-05-21 10:47:53 -0700

    spelling

--- src/pages/analytics.js
+++ src/pages/analytics.js
@@ -2,10 +2,9 @@ import Image from "next/image";
 import Navbar from "components/Navbar";
 import "../../app/globals.css";
 
-
 const captionTitleStyle = {
-  color: 'blue',
-  textDecoration: 'underline'
+  color: "blue",
+  textDecoration: "underline",
 };
 
 export default function About() {
@@ -14,97 +13,106 @@ export default function About() {
       <Navbar />
       <main className="flex flex-col items-center justify-between p-2">
         <div className="container">
-          <div className="text-5xl pb-7 font-semibold ">Yolo County Data Analysis</div>
+          <div className="text-5xl pb-7 font-semibold ">
+            Yolo County Data Analysis
+          </div>
           <br />
-          <div className="text-lg" >
-    We wanted to answer the question: To what extent does Mental Health play a role in the Davis Community?   
- <br />
- <br />
-    We used Matplotlib and Jyputer Notebook to generate data visualizations to better understand this role.
-     
-           </div>
-     <br />
-      <h1><a style={captionTitleStyle} href="https://github.com/ritvikir/hackdavis/blob/main/public/Narrative.pdf">One Page Narrative</a></h1>
-        <h1><a style={captionTitleStyle} href="https://github.com/ritvikir/hackdavis/blob/main/visualizationMentalHealth.ipynb">Source Code</a></h1>
-        
-
+          <div className="text-lg">
+            We wanted to answer the question: To what extent does Mental Health
+            play a role in the Davis Community?
+            <br />
+            <br />
+            We used Matplotlib and Jupyter Notebook to generate data
+            visualizations to better understand this role.
+          </div>
+          <br />
+          <h1>
+            <a
+              style={captionTitleStyle}
+              href="https://github.com/ritvikir/hackdavis/blob/main/public/Narrative.pdf"
+            >
+              One Page Narrative
+            </a>
+          </h1>
+          <h1>
+            <a
+              style={captionTitleStyle}
+              href="https://github.com/ritvikir/hackdavis/blob/main/visualizationMentalHealth.ipynb"
+            >
+              Source Code
+            </a>
+          </h1>
         </div>
-        
       </main>
-      
+
       <div className="container mx-auto px-4">
-      <div className="flex items-center justify-center">
-        <ImageWithCaption
-          src="data1.png"
-          alt="Data Visualization 1"
-          caption="There is a clear increase in health service required as kids grow older."
-        />
-         
+        <div className="flex items-center justify-center">
+          <ImageWithCaption
+            src="data1.png"
+            alt="Data Visualization 1"
+            caption="There is a clear increase in health service required as kids grow older."
+          />
+        </div>
+        <div className="flex items-center justify-center">
+          <ImageWithCaption
+            src="data2.png"
+            alt="Data Visualization 1"
+            caption="There is a polarizing difference in the number of kids by age."
+          />
+        </div>
+        <div className="flex items-center justify-center">
+          <ImageWithCaption
+            src="data3.png"
+            alt="Data Visualization 1"
+            caption="There is a clear correlation between 1 MHS and 5 MHS."
+          />
+        </div>
+        <div className="flex items-center justify-center">
+          <ImageWithCaption
+            src="data4.png"
+            alt="Data Visualization 1"
+            caption="There is a clear uptick in the services required by a teenager. "
+          />
+        </div>
+        <div className="flex items-center justify-center">
+          <ImageWithCaption
+            src="data5.png"
+            alt="Data Visualization 1"
+            caption="The data collection is fair amongst all age groups."
+          />
+        </div>
+        <div className="flex items-center justify-center">
+          <ImageWithCaption
+            src="data6.png"
+            alt="Data Visualization 1"
+            caption="The heatmap shows that majority of youth require some MHS."
+          />
+        </div>
+        <div className="flex items-center justify-center">
+          <ImageWithCaption
+            src="data7.png"
+            alt="Data Visualization 1"
+            caption="This violin plot shows that the total count stays standard by age."
+          />
+        </div>
       </div>
-      <div className="flex items-center justify-center">
-        <ImageWithCaption
-          src="data2.png"
-          alt="Data Visualization 1"
-          caption="There is a polarizing difference in the number of kids by age."
-        />
-         
-      </div>
-      <div className="flex items-center justify-center">
-        <ImageWithCaption
-          src="data3.png"
-          alt="Data Visualization 1"
-          caption="There is a clear correlation between 1 MHS and 5 MHS."
-        />
-         
-      </div>
-      <div className="flex items-center justify-center">
-        <ImageWithCaption
-          src="data4.png"
-          alt="Data Visualization 1"
-          caption="There is a clear uptick in the services required by a teenager. "
-        />
-         
-      </div>
-      <div className="flex items-center justify-center">
-        <ImageWithCaption
-          src="data5.png"
-          alt="Data Visualization 1"
-          caption="The data collection is fair amongst all age groups."
-        />
-         
-      </div>
-      <div className="flex items-center justify-center">
-        <ImageWithCaption
-          src="data6.png"
-          alt="Data Visualization 1"
-          caption="The heatmap shows that majority of youth require some MHS."
-        />
-         
-      </div>
-      <div className="flex items-center justify-center">
-        <ImageWithCaption
-          src="data7.png"
-          alt="Data Visualization 1"
-          caption="This violin plot shows that the total count stays standard by age."
-        />
-         
-      </div>
-      
-    </div>
-
-
     </div>
   );
 }
 
-
-
 const ImageWithCaption = ({ src, alt, caption }) => {
   return (
     <div className="w-1/2 p-4 pb-4">
-      <br/>
+      <br />
       <img src={src} alt={alt} className="w-full mx-auto" />
-      {caption && <p style={{color: '#75e07e'}} className="text-center font-semibold pt-2 mb-5">{caption}</p>}
+      {caption && (
+        <p
+          style={{ color: "#75e07e" }}
+          className="text-center font-semibold pt-2 mb-5"
+        >
+          {caption}
+        </p>
+      )}
     </div>
   );
 };
